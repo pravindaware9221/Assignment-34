cdfd520f34d460cd69eddbbead44638fbb606bf3
Pravin <pravindaware9221@gmail.com>
2024-08-12 22:39:04 +0530

    add for loop program and modifed 1st & 2nd program

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Find largest number
 const a = [1,50,45,6,3,252,88,58,68,755,];
 const b = Math.max(...a);
-console.log(b);
+const d = Math.min(...a)
+console.log('Largest Number ',b);
+console.log('Small Number',d)
 
 
 // even odd 
 const c = a.map(num => num % 2 === 0 ? `${num} Even` : `${num} Odd`);
-console.log(b);
+console.log(c);
 
 
+// for loop
+const arr =[ 'Ram' , 'sham' ,'Radha' ,'Krishna' ,'om']
+for( let i=0 ; i < arr.length ; i++){
+console.log(`Hello,${arr[i]}`);
+}
