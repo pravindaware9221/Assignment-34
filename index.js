e653ebf798de3ee5965b18d517b1681c21f86419
Pravin <pravindaware9221@gmail.com>
2024-08-12 22:29:48 +0530

    add even odd program

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Find largest number
 const a = [1,50,45,6,3,252,88,58,68,755,];
 const b = Math.max(...a);
 console.log(b);
+
+
+// even odd 
+const c = a.map(num => num % 2 === 0 ? `${num} Even` : `${num} Odd`);
+console.log(b);
+
+
